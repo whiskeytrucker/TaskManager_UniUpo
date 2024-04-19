303ccba9cb35dbef28e63baf621177e66a195171
Pole-Desktop <56837977+whiskeytrucker@users.noreply.github.com>
2024-04-19 18:40:39 +0200

    Item dentro la RecyclerView reso cliccabile, da: - rendere scuro una volta cliccato - far partire un'altro adapter per la stessa RecyclerView

--- app/src/main/java/it/polettomatteo/taskmanager_uniupo/adapters/ProjectsAdapter.kt
+++ app/src/main/java/it/polettomatteo/taskmanager_uniupo/adapters/ProjectsAdapter.kt
@@ -1,16 +1,26 @@
 package it.polettomatteo.taskmanager_uniupo.adapters
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import it.polettomatteo.taskmanager_uniupo.R
 import it.polettomatteo.taskmanager_uniupo.dataclass.Project
 
+
+val TAG = "ProjectsAdapter"
 class ProjectsAdapter(private var dataSet: ArrayList<Project>) : RecyclerView.Adapter<ProjectsAdapter.ViewHolder>(){
 
 
@@ -20,6 +30,7 @@ class ProjectsAdapter(private var dataSet: ArrayList<Project>) : RecyclerView.Ad
         val assigned: TextView
         val progress: TextView
         val seekBar: SeekBar
+
 
         init {
             title = view.findViewById(R.id.title)
@@ -33,20 +44,20 @@ class ProjectsAdapter(private var dataSet: ArrayList<Project>) : RecyclerView.Ad
 
             val perc = seekBar.progress
 
-            progress.offsetLeftAndRight(perc)
-
         }
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // crea una nuova view, dove definisce la ui dell'elemento della lista
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.rv_row_item, parent, false)
+            .inflate(R.layout.project_item, parent, false)
 
         return ViewHolder(view)
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "ClickableViewAccessibility")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // prendi l'elemento dal dataset e rimpazza i contenuti
         holder.title.text = dataSet[position].titolo
@@ -55,6 +66,14 @@ class ProjectsAdapter(private var dataSet: ArrayList<Project>) : RecyclerView.Ad
         holder.progress.text = "${dataSet[position].progress}%"
         holder.seekBar.progress = dataSet[position].progress
 
+        val perc = dataSet[position].progress
+
+        val view = holder.itemView
+
+        holder.itemView.setOnClickListener {view.setOnClickListener {
+            Log.d(TAG, "Hai premuto la View, dio canaglia")
+            //
+        }}
     }
 
 
